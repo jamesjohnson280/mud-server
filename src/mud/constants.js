f91be421db5f9bed096340dd39113906334cf046
James Johnson <james.johnson280@gmail.com>
2022-01-18 11:19:46 -0500

    Show example values

--- src/mud/constants.js
+++ src/mud/constants.js
@@ -2,10 +2,10 @@
  * Global constants
  * @module constants */
 
-/** The title of the Mud */
+/** The title of the Mud: 'SocketMud' */
 const Title = 'SocketMud';
 
-/** The version of the Mud */
+/** The version of the Mud: '0.1' */
 const Version = '0.1';
 
 export { Title, Version };
