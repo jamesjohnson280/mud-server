6f20a2a5e819faf6ca2ffcebcdf8d845a433b151
James Johnson <james.johnson280@gmail.com>
2022-01-19 12:38:21 -0500

    Refactor game tests

--- src/mud/game.test.js
+++ src/mud/game.test.js
@@ -2,22 +2,22 @@ import { handlePlayerInput } from './game';
 import { World } from './World';
 import { rooms } from './data/rooms';
 
+const world = new World(rooms);
+const key = { id: 'jim' };
+
 describe('game', () => {
   test('it registers new players', () => {
-    const message = handlePlayerInput(new World(rooms), {}, 'Jim');
+    const message = handlePlayerInput(world, key, 'Jim');
     expect(/Hello, Jim./gi.test(message.self)).toBeTruthy();
   });
 
   test('after registering, it sends the player to the starting room', () => {
-    const world = new World(rooms);
-    world.players.set({ id: 'jim' }, { name: 'Jim' });
-    const message = handlePlayerInput(world, { id: 'jim' }, 'Jim');
+    world.players.set(key, { name: '' });
+    const message = handlePlayerInput(world, key, 'Jim');
     expect(/Dirt Road/gi.test(message.self)).toBeTruthy();
   });
 
   test('it handles registered player input', () => {
-    const world = new World();
-    const key = { id: 'jim' };
     world.players.set(key, { name: 'Jim' });
     const message = handlePlayerInput(world, key, 'hi');
     expect(message.self).toBe('hi');
