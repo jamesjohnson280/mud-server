f117e1c3085602b0bf7bcdcfa49392ee7a9047fb
James Johnson <james.johnson280@gmail.com>
2022-01-21 15:47:57 -0500

    Refactor jest to use babel

--- jest.config.cjs
+++ jest.config.cjs
@@ -173,7 +173,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '.js': 'jest-esm-transformer'
+    '^.+\\.[t|j]sx?$': 'babel-jest'
   }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
