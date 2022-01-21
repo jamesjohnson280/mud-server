d1647d677ee8524cb551d80a6b2cedd7582f6504
James Johnson <james.johnson280@gmail.com>
2022-01-21 14:26:53 -0500

    Document the emote command

--- src/mud/commands.js
+++ src/mud/commands.js
@@ -3,6 +3,13 @@
  * @module commands
  */
 
+/**
+ * Broadcasts an emote to every player in a room
+ * @param {string} verb The verb used to invoke the command
+ * @param {string} args The text after the verb in the player's input string
+ * @param {object} context An object containing the player, world and other contextual data
+ * @returns And object containing the outcome of the command to be broadcast to the players
+ */
 function emote(verb, args, context) {
   const { player } = context;
   if (!args) {
